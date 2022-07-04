5e507fed942c69b476261ae92f360e0e16aab4a3
unknown <parthtyagi4@gmail.com>
2022-07-04 12:21:32 +0530

    first commit

---
+++ PortfolioManagement-master/PortfolioManagement-master/AutheticationAPITest/UserServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthenticationAPI.Models;
+using AuthenticationAPI.Service;
+using Moq;
+using NUnit.Framework;
+
+namespace AutheticationAPITest
+{
+    [TestFixture]
+    class UserServiceTest
+    {
+        private Mock<IUserService> mock;
+        LoginCredentials loginCredentials;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mock = new Mock<IUserService>();
+            loginCredentials = new LoginCredentials("User", "User");
+        }
+        [Test]
+        public void AuthenticateUser_Return_UserCredentials()
+        {
+            mock.Setup(x => x.AuthenticateUser(loginCredentials)).Returns(loginCredentials);
+            LoginCredentials user = mock.Object.AuthenticateUser(loginCredentials);
+            Assert.AreEqual(loginCredentials, user);
+        }
+    }
+}
